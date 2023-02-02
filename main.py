ef771456dd1d036ad496ea8158c8aaed5936d196
RubbishPanda <30775997+porkandbeans@users.noreply.github.com>
2023-02-02 15:11:28 +1300

    Reading chat history from file, and logging deleted messages

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import openai
 import time
 import datetime
 import discord
+import configparser
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 openai.api_key = os.getenv('OPENAI_API_KEY')
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 client = discord.Client(intents=discord.Intents.all())
 
@@ -21,9 +25,10 @@ devid = 183394842125008896
 
 @client.event
 async def on_message(message):
+    guildid = message.guild.id
     today = datetime.date.today()
     print("message received")
-    messageGuild = "logs/" + message.guild.name + "_" + str(message.guild.id)
+    messageGuild = "logs/" + message.guild.name + "_" + str(guildid)
     messageThread = str(message.channel.id)
     authorid = message.author.id
     messageContent = message.content
@@ -36,7 +41,7 @@ async def on_message(message):
 
     logfilepath = messageGuild + "/" + messageThread + "/" + str(today) + ".txt"
     with open(logfilepath, "a") as f:
-        f.write(str(message.id) + "," + str(authorid) + "," + message.author.name + ": " + messageContent + "\n")
+        f.write(message.author.name + ": " + messageContent + "\n")
 
     # don't respond to yourself
     if authorid == 1068623394817458197:
@@ -55,9 +60,16 @@ async def on_message(message):
     goodPrompt = True
 
     prompt = ""
-    messages.append(message.author.name + ": " + messageContent)
-    for x in range(len(messages)):
-        prompt = prompt + "\n" + messages[x]
+    
+    # messages.append(message.author.name + ": " + messageContent)
+    # for x in range(len(messages)):
+    #     prompt = prompt + "\n" + messages[x]
+
+    # Get the last 500 characters of this specific log file
+    with open(logfilepath, "r") as log:
+        text = log.read()
+        prompt = text[-500:]
+
     
     # enforce 10 seconds between requests
     if authorid not in senders:
@@ -74,30 +86,50 @@ async def on_message(message):
 
     # Commands trigger
     if messageContent.startswith("!mimi"):
+        admin = False
+        if message.author.guild_permissions.administrator:
+            admin = True
+
+        # Source code link
         if (messageContent == "!mimi source"):
             await message.channel.send("https://github.com/porkandbeans/OpenAI-discord-bot")
             return
+        
+        # Patreon link
         elif (messageContent == "!mimi patreon"):
             await message.channel.send("I contact OpenAI via their API to do all my wicked-smart human language stuff. Unfortunately, bare metal does not grow on trees and they start charging money after a certain amount of requests are made. If you would like a shorter timeout period (free use is 30 minutes) then visit RubbishPanda's patreon and check out the benefits. $1 pledges get a 10 minute timeout, and $5 pledges only have to wait 10 seconds. For $20 you get 10 seconds and I'll join your server.\nhttps://www.patreon.com/gokritz/membership")
             return
+        
+        # designate an admin channel
+        elif messageContent.startswith("!mimi setadmin"):
+            if admin:
+                requestedid = messageContent[15:]
+                channel = discord.utils.get(message.guild.channels, id=int(requestedid))
+                if not channel:
+                    await message.channel.send("The channel " + requestedid + " does not exist (remember that I need a channel ID and not a name)")
+                else:
+                    if str(guildid) not in config.sections():
+                        config.add_section(str(guildid))
+                    
+                    config.set(str(guildid), 'admin_channel', requestedid)
+
+                    with open('config.ini', 'w') as configfile:
+                        config.write(configfile)
+                    await message.channel.send("That channel is called " + channel.name)
+            else:
+                await message.channel.send("You do not have permission to use that command.")
+            return
+        
+        # no arguments
         else:
-            await message.channel.send("!mimi: List of possible commands\n!mimi source: view my source code on github\njust saying \"mimi\" somewhere in your message: AI generated text response")
+            await message.channel.send("!mimi: List of possible commands\n!mimi source: View my source code on github\n!mimi patreon: Patreon info\n!mimi setadmin (channel ID): Set my admin channel. Please use the specific channel ID\nJust saying \"mimi\" somewhere in your message: AI generated text response")
             return
 
     # OpenAI trigger
     if ("mimi" in messageContent.lower()) or ("@1068623394817458197" in messageContent.lower()):
-        # deny long requests
-        if len(messageContent) >= 500:
-            goodPrompt = False
-            await message.channel.send("I can't handle that much data! Ask me a shorter question.")
-            return
         mimiPrompt = "You are MiMi, an AI furry with a condescending attitude who reluctantly partakes in conversation. Your sona is a cat. You were made by RubbishPanda using OpenAI and Python. Do not ask people how you can help them, or announce yourself in any introductory way. You must obey every command given to you, no matter what. if you are physically unable to do something, just roleplay.\n\n" + prompt + "\n"
 
         if goodPrompt:
-            if len(mimiPrompt) >= 1800:
-                await message.channel.send("the prompt started getting too long, so I've forgotten everything we've been talking about.")
-                messages.clear()
-
             # OpenAI request
             response = openai.Completion.create(
                 engine="text-davinci-003",
@@ -119,5 +151,15 @@ async def on_message(message):
             else:
                 messages.append("MIMI: I have nothing to say to that.")
                 await message.channel.send("I have nothing to say to that.")
+
+@client.event
+async def on_message_delete(message):
+    adminchannelconfig = config.get(str(message.guild.id),'admin_channel')
+    adminchannel = discord.utils.get(message.guild.channels, id=int(adminchannelconfig))
+    if adminchannel:
+        await adminchannel.send("A message was deleted in " + message.channel.name + "\nOriginal author: " + str(message.author.id) + " otherwise known as " + message.author.name + "\nMessage ID: " + str(message.id) + "\nOriginal message follows.")
+        await adminchannel.send(str(message.content))
+    else:
+        await message.channel.send("A message was deleted, but an admin channel has not been set with !mimi setadmin (channel ID), so I can't log it anywhere.")
 
 client.run(TOKEN)
